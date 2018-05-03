e7c2ec5aa164120b962d6a0c592509e3c18af229
weaming <garden.yuen@gmail.com>
2018-05-03 22:12:09 +0800

    update test

--- extension/rss_test.go
+++ extension/rss_test.go
@@ -3,24 +3,17 @@ package extension
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 const (
 	WANQU = "https://wanqu.co/feed"
 )
 
-func TestRSS(t *testing.T) {
-	content, err := parseFeed(WANQU, true, ItemParseLink)
-	if err != nil {
-		t.Error(err)
-	} else {
-		fmt.Println(content)
-	}
-}
+var rss = NewRSSPool(time.Duration(10000), false)
 
-func TestWanqu(t *testing.T) {
-	content, err := parseFeed(WANQU, true, ItemParseWanquDaily)
-	fmt.Println(content)
+func TestRSS(t *testing.T) {
+	content, err := rss.parseFeed(WANQU, "123", true, ItemParseLink)
 	if err != nil {
 		t.Error(err)
 	} else {
